c7af804b59b0e1cb422bebc010322bbaedee19a0
Nishanth M <innocentnishanth@gmail.com>
2023-07-27 12:29:51 +0530

    Added Initial Code

---
+++ backend/app.js
@@ -0,0 +1,84 @@
+// START OF SETUP ENVIRONMENT VARIABLES
+const dotenv = require('dotenv');
+const path = require('path');
+dotenv.config({ path: path.join(__dirname, 'config', 'config.env') });
+// END OF SETUP ENVIRONMENT VARIABLES
+
+const express = require('express');
+const cors = require('cors');
+const logger = require('morgan');
+const errorMiddleware = require('./middleware/error');
+const app = express();
+const helmet = require('helmet');
+const cookieParser = require('cookie-parser');
+// setup body parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Cookie parser middleware
+app.use(cookieParser());
+
+// CORS middleware
+const corsOptions = {
+    origin: 'http://localhost:5173',
+    credentials: true, //access-control-allow-credentials:true
+    optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+
+// Logger middleware
+app.use(logger('dev'));
+
+// Basic security middleware
+app.use(helmet());
+// Avoid Fingerprinting
+app.disable('x-powered-by');
+
+// =============================== SWAGGER  ==================================
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+const options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'API Documentation',
+            version: '1.0.0',
+        },
+        servers: [
+            {
+                url: 'http://localhost/3500',
+            },
+        ],
+    },
+    apis: ['./routes/*.js'],
+};
+
+const swaggerSpec = swaggerJsDoc(options);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+// =============================== end of SWAGGER  ==================================
+
+// Setup Root route
+
+app.use('/$', (req, res) => {
+    return res.status(200).json({
+        success: true,
+        message: 'Welcome to the project',
+    });
+});
+
+app.use('/api/v1', require('./routes/auth.routes'));
+app.use('/api/v1', require('./routes/user.routes'));
+
+// 404 page
+app.all('*', (req, res) => {
+    return res.status(404).json({
+        success: false,
+        message: 'Path not found',
+    });
+});
+
+// Error handling middleware
+app.use(errorMiddleware);
+
+module.exports = app;
---
+++ backend/routes/auth.routes.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+const rateLimit = require('express-rate-limit');
+
+const {
+    registerUser,
+    loginUser,
+    logoutUser,
+} = require('../controllers/auth.controllers');
+
+const limiter = rateLimit({
+    windowMs: 1 * 60 * 1000, // 1 min
+    max: 3, // 3 requests per windowMs
+    message: 'Too many requests from this IP, please try again later.',
+});
+
+// AUTH ROUTES
+router.route('/register').post(registerUser);
+router.route('/login').post([limiter, loginUser]);
+router.route('/logout').get(logoutUser);
+
+module.exports = router;
