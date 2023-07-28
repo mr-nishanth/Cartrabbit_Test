792d97e12ee916df3bac796c5310d78fce6cb61a
Nishanth M <innocentnishanth@gmail.com>
2023-07-28 18:28:34 +0530

    Added Auth and Room Store

--- frontend/src/store/useAuthStore.ts
+++ frontend/src/store/useAuthStore.ts
@@ -2,9 +2,6 @@ import { create } from 'zustand';
 import request from '../utils/axios_request';
 import endpoints from '../utils/enpoints';
 
-// import { devtools, persist } from 'zustand/middleware';
-// const myMiddlewares = (f) => devtools(persist(f));
-
 const initialState: AuthStore = {
     isAuthenticated: false,
     user: null,
@@ -13,37 +10,33 @@ const initialState: AuthStore = {
 
 interface AuthAction {
     register: (
-        username: string,
+        name: string,
         password: string,
         email: string,
-        mobile: string
+        mobile: string,
+        role: string
     ) => Promise<Response>;
     login: (email: string, password: string) => Promise<Response>;
     logout: () => Promise<boolean | undefined>;
     getProfile: () => Promise<User>;
-    updateProfile: (
-        username: string,
-        email: string,
-        mobile: string
-    ) => Promise<Response>;
-    getAllUser: () => Promise<User[]>;
-    deleteUser: (id: string) => Promise<Response>;
 }
 
-const useAuthStore = create<AuthStore & AuthAction>()((set, get) => ({
+const useAuthStore = create<AuthStore & AuthAction>()((set) => ({
     ...initialState,
     register: async (
-        username: string,
+        name: string,
         password: string,
         email: string,
-        mobile: string
+        mobile: string,
+        role: string
     ) => {
         try {
             const { data } = await request.post(endpoints.register, {
-                username,
+                name,
                 password,
                 email,
                 mobile,
+                role,
             });
             console.log({ REGISTER_RESPONSE: data });
             if (data.success) {
@@ -69,8 +62,7 @@ const useAuthStore = create<AuthStore & AuthAction>()((set, get) => ({
             if (data.success) {
                 set({
                     isAuthenticated: true,
-
-                    user: data.userObj,
+                    user: data?.user,
                 });
                 return data;
             }
@@ -110,64 +102,6 @@ const useAuthStore = create<AuthStore & AuthAction>()((set, get) => ({
             }
         } catch (error: any) {
             console.error('Get Profile failed:', error);
-            console.log({ error });
-            return error?.response?.data;
-        }
-    },
-
-    updateProfile: async (username: string, email: string, mobile: string) => {
-        try {
-            const { data } = await request.put(endpoints.updateProfile, {
-                name: username,
-                email,
-                mobile,
-            });
-            console.log({ UPDATE_PROFILE_RESPONSE: data });
-            if (data.success) {
-                set({
-                    user: data?.user,
-                });
-                return data;
-            }
-        } catch (error: any) {
-            console.error('Update Profile failed:', error);
-            console.log({ error });
-            return error?.response?.data;
-        }
-    },
-
-    // Admin
-    getAllUser: async () => {
-        try {
-            const { data } = await request.get(endpoints.getAllUser);
-            console.log({ GET_ALL_USER_RESPONSE: data });
-            if (data.success) {
-                set({
-                    users: data?.users,
-                });
-                return data;
-            }
-        } catch (error: any) {
-            console.error('Get all User failed:', error);
-            console.log({ error });
-            return error?.response?.data;
-        }
-    },
-
-    deleteUser: async (id: string) => {
-        try {
-            const { data } = await request.delete(
-                `${endpoints.deleteUser}/${id}`
-            );
-            console.log({ DELETE_RESPONSE: data });
-            if (data.success) {
-                let newUsers = get().users!;
-                newUsers = newUsers?.filter((user) => user._id !== id);
-                set((state) => ({ ...state, users: newUsers }));
-                return data;
-            }
-        } catch (error: any) {
-            console.error('Delete User failed:', error);
             console.log({ error });
             return error?.response?.data;
         }
